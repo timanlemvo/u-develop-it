c3a4f158b7fa64b0f3043be031e4eeb080485876
Tima Nlemvo <tnlemvo@AML-TNLEMVO.caa.comgit commit -m added>
2021-06-30 17:33:17 -0700

    updated server.js again

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -9,6 +11,23 @@ app.use(express.json());
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
+  });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '88Gkstone!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
   });
 
 // function that will start the Express.js server on port 3001
